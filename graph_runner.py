263a6a4130dd7d616ac4abbc07ffb65cc2f411ef
Nick <0x0b4ac@gmail.com>
2025-10-24 16:44:40 +0800

    send to discord

--- graph_runner.py
+++ graph_runner.py
@@ -1,33 +1,55 @@
 from langgraph.graph import StateGraph, END
 from typing import TypedDict, List
+import os
+import requests
 from news_sources import fetch_hackernews
 from translator import translate_titles
 from formatter import format_markdown
+
 
 class NewsState(TypedDict):
     items: List[dict]
     translated: List[str]
     markdown: str
 
+
 def node_fetch_hn(state: NewsState):
     items = fetch_hackernews()
     return {"items": items}
+
 
 def node_translate(state: NewsState):
     titles = [item['title'] for item in state['items']]
     return {"translated": translate_titles(titles)}
 
+
 def node_format(state: NewsState):
     return {"markdown": format_markdown("Hacker News", state['items'], state['translated'])}
+
+
+def node_send_discord(state: NewsState):
+    webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+    if not webhook_url:
+        print("DISCORD_WEBHOOK_URL not set, skipping notification.")
+        return
+
+    markdown_content = state["markdown"]
+    # send to discord channel in chunks of 1900 characters
+    for i in range(0, len(markdown_content), 1900):
+        chunk = markdown_content[i:i+1900]
+        requests.post(webhook_url, json={"content": chunk})
+
 
 def build_graph():
     builder = StateGraph(NewsState)
     builder.add_node("fetch_hn", node_fetch_hn)
     builder.add_node("translate", node_translate)
     builder.add_node("format", node_format)
+    builder.add_node("send_discord", node_send_discord)
 
     builder.set_entry_point("fetch_hn")
     builder.add_edge("fetch_hn", "translate")
     builder.add_edge("translate", "format")
-    builder.add_edge("format", END)
+    builder.add_edge("format", "send_discord")
+    builder.add_edge("send_discord", END)
     return builder.compile()
