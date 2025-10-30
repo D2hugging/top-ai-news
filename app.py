118f8f4af949088caa34c92fe2ef9412e2f38c4e
Nick <0x0b4ac@gmail.com>
2025-10-30 20:19:10 +0800

    comment

--- app.py
+++ app.py
@@ -61,6 +61,6 @@ iface = gr.Interface(
 
 app = mount_gradio_app(app, iface, path="/")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=7860)
